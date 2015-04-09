bc7a9cfc9c972b18a2af974458b8132bd64e81ca
Daniel Shuldman <eliasds@gmail.com>
2015-04-09 00:40:25 -0700

    Duets and Quartets completed.

--- AdvancedPhaseRetrieval.m
+++ AdvancedPhaseRetrieval.m
@@ -1,6 +1,6 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Left - Right
+% Left - Right (Duets)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [~,J]=ismember(idx_left,idx_bf); J(J==0) = []; bfleft = 0; for L=idx_bf(J)'; bfleft = bfleft + Imea(:,:,L); end; figure(98); imagesc(real(bfleft)); colormap gray; colorbar; axis image; axis ij; title(L); drawnow
 title('Bright Field (Left)')
@@ -12,7 +12,7 @@ figure(100); imagesc(real(bfleft-bfright)); colormap gray; colorbar; axis image;
 title('Bright Field (Left - Right)')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Top - Bottom
+% Top - Bottom (Duets)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [~,J]=ismember(idx_top,idx_bf); J(J==0) = []; bftop = 0; for L=idx_bf(J)'; bftop = bftop + Imea(:,:,L); end; figure(198); imagesc(real(bftop)); colormap gray; colorbar; axis image; axis ij; title(L); drawnow
 title('Bright Field (Top)')
@@ -24,17 +24,47 @@ figure(200); imagesc(real(bftop-bfbottom)); colormap gray; colorbar; axis image;
 title('Bright Field (Top - Bottom)')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Diagonal
+% Diagonal (Quartets)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-[~,J]=ismember(idx_bottom,idx_bf); J(J==0) = []; bfbottom = 0; for L=idx_bf(J)'; bfbottom = bfbottom + Imea(:,:,L); end; figure(198); imagesc(real(bfbottom)); colormap gray; colorbar; axis image; axis ij; title(L); drawnow
-title('Bright Field (ULeftBRight)')
+[~,J]=ismember(idx_top,idx_left); J(J==0) = [];
+[~,J]=ismember(idx_left(J),idx_bf); J(J==0) = [];
+[~,K]=ismember(idx_bottom,idx_right); K(K==0) = [];
+[~,K]=ismember(idx_right(K),idx_bf); K(K==0) = [];
+J = cat(1,J,K);
+bftlbr = 0; for L=idx_bf(J)'; bftlbr = bftlbr + Imea(:,:,L); end; figure(298); imagesc(real(bftlbr)); colormap gray; colorbar; axis image; axis ij; title(L); drawnow
+title('Bright Field (TopLeftBottomRight)')
 
-[~,J]=ismember(idx_top,idx_bf); J(J==0) = []; bftop = 0; for L=idx_bf(J)'; bftop = bftop + Imea(:,:,L); end; figure(199); imagesc(real(bftop)); colormap gray; colorbar; axis image; axis ij; title(L); drawnow
-title('Bright Field (URightBLeft)')
+[~,J]=ismember(idx_top,idx_right); J(J==0) = [];
+[~,J]=ismember(idx_right(J),idx_bf); J(J==0) = [];
+[~,K]=ismember(idx_bottom,idx_left); K(K==0) = [];
+[~,K]=ismember(idx_left(K),idx_bf); K(K==0) = [];
+J = cat(1,J,K);
+bftrbl = 0; for L=idx_bf(J)'; bftrbl = bftrbl + Imea(:,:,L); end; figure(299); imagesc(real(bftrbl)); colormap gray; colorbar; axis image; axis ij; title(L); drawnow
+title('Bright Field (TopRightBottomLeft)')
 
-figure(200); imagesc(real(bftop-bfbottom)); colormap gray; colorbar; axis image; axis ij; title(L); drawnow
-title('Bright Field (ULeftBRight - URightBLeft)')
+figure(300); imagesc(real(bftlbr-bftrbl)); colormap gray; colorbar; axis image; axis ij; title(L); drawnow
+title('Bright Field (TopLeftBottomRight - TopRightBottomLeft)')
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Sextets (Under Construction)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[~,J]=ismember(idx_top,idx_left); J(J==0) = [];
+[~,J]=ismember(idx_left(J),idx_bf); J(J==0) = [];
+[~,K]=ismember(idx_bottom,idx_right); K(K==0) = [];
+[~,K]=ismember(idx_right(K),idx_bf); K(K==0) = [];
+J = cat(1,J,K);
+bftlbr = 0; for L=idx_bf(J)'; bftlbr = bftlbr + Imea(:,:,L); end; figure(298); imagesc(real(bftlbr)); colormap gray; colorbar; axis image; axis ij; title(L); drawnow
+title('Bright Field (TopLeftBottomRight)')
 
+[~,J]=ismember(idx_top,idx_right); J(J==0) = [];
+[~,J]=ismember(idx_right(J),idx_bf); J(J==0) = [];
+[~,K]=ismember(idx_bottom,idx_left); K(K==0) = [];
+[~,K]=ismember(idx_left(K),idx_bf); K(K==0) = [];
+J = cat(1,J,K);
+bftrbl = 0; for L=idx_bf(J)'; bftrbl = bftrbl + Imea(:,:,L); end; figure(299); imagesc(real(bftrbl)); colormap gray; colorbar; axis image; axis ij; title(L); drawnow
+title('Bright Field (TopRightBottomLeft)')
+
+figure(300); imagesc(real(bftlbr-bftrbl)); colormap gray; colorbar; axis image; axis ij; title(L); drawnow
+title('Bright Field (TopLeftBottomRight - TopRightBottomLeft)')
 
 
