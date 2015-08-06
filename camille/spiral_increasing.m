e0618d31fcf9b28712a3e34cdfae99f25c062b40
eliasds <eliasds@gmail.com>
2015-08-06 12:05:18 -0700

    spiral_increasing.m: added extra parameters

--- camille/spiral_increasing.m
+++ camille/spiral_increasing.m
@@ -3,16 +3,19 @@ figure()
 p = 12;
 n = 2000;
 size_w = 750; % size of window
-a1 = 0.1;
+a1 = 1E-2;
+b1 = 3;
+a2 = 1E-2;
+b2 = b1;
 linecolor = 'k'; % color of spiral
 
 %Simple Spiral
 t = linspace(0,p*pi,n);
-x_1 = -a1*t.^3.*sin(t);
-y_1 = -a1*t.^3.*cos(t);
+x_1 = -1*a1*t.^b1.*sin(t);
+y_1 = -1*a1*t.^b1.*cos(t);
 
-x_2 = a1*t.^3.*sin(t);
-y_2 = a1*t.^3.*cos(t);
+x_2 = a2*t.^(b2).*sin(t);
+y_2 = a2*t.^(b2).*cos(t);
 
 hold on
 
@@ -22,5 +25,7 @@ set(gca, 'Units', 'Pixels');
 set(gca, 'Position', [1 1 size_w+1 size_w+1])
 set(gcf, 'OuterPosition', [0 0 size_w+18 size_w+110])
 axis([-size_w/2 size_w/2 -size_w/2 size_w/2]);
+handle = title(['p=',num2str(p),' n=',num2str(n),' sizew=',num2str(size_w),' a1=',num2str(a1),' b1=',num2str(b1),' a2=',num2str(a2),' b2=',num2str(b2)]);
+set(handle,'Position',[0,100]);
 % axis image
-% axis off
+axis off
