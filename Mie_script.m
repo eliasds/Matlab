bb534132c111f8969b1ee41f5c517fb72005956f
Daniel Shuldman <eliasds@gmail.com>
2015-11-02 16:05:02 -0800

    Mie_script.m: added mask stuff and what gets saved

--- Mie_script.m
+++ Mie_script.m
@@ -5,7 +5,8 @@ clear all
 
 %% system parameters: NA needs to be large enough
 % size of hologram
-N = 2048;
+N = 2048-200;
+zpad = N+200;
 %Magnification
 mag=4;
 % pixel size
@@ -14,7 +15,7 @@ dpix = ps/mag; % in meters
 % number of particles
 Np = 10;
 % Use a Coded Aperture Mask
-maskon=true;
+maskon = false;
 
 % random particle size between dmin and dmax in meters
 dmin = 10E-6;
@@ -31,12 +32,22 @@ n1 = 1.33;   % index of refraction of water
 n2 = 1.5821; %polystyrene
 n3 = 1.46; %cellulose
 
+%Filename to save to
+fn = ['Mie',num2str(N),'px_',num2str(Np),'part_',num2str(round(n1*100)),'n1_',num2str(round(n2*100)),'n2'];
+
+
 %% Coded Aperture Mask
-mask=ones(N,N);
-mask(1:end,1:N/2-1)=0;
-% mask=flipud(mask);
-% mask=fliplr(mask);
-maskpadded = imresize(mask,'scale',2,'method','nearest');
+mask = 1;
+maskpadded = 1;
+if maskon == true
+    mask = ones(N,N);
+    mask(1:end,1:N/2-1) = 0;
+%     mask(1:N/2-1,1:N/2-1) = 0;
+    mask = rot90(mask,0);
+    maskpadded = imresize(mask, zpad/N, 'method', 'nearest', 'colormap', 'original');
+    fn = ['Mie',num2str(N),'px_',num2str(Np),'part_',num2str(round(n1*100)),'masked_n1_',num2str(round(n2*100)),'n2'];
+
+end
 
 lambda = 0.6328E-6; % in meters
 k = 2*pi/lambda*n1;
@@ -44,9 +55,6 @@ k = 2*pi/lambda*n1;
 % crude oil
 % n2 = 1.5+100i; % index of refraction of the sphere
 % air bubble, oil drop, particle
-
-% delta_x = lambda/NA % lateral resolution
-% DOF = lambda/NA^2 % depth of focus
 
 %%
 % particle z locations randomly between zmin and zmax (in meters)
@@ -57,29 +65,45 @@ zres = 0.25E-3; % resolution in meters
 z_obj = round((rand([1,Np])*(zmax-zmin)+zmin)/zres)*zres; %distance range from 5 to 15
 % Put the first particle in a specific z-position 
 z_obj(1) = 2.5E-3; %in meters
+% z_obj(2) = -1.5E-3; %in meters
+% z_obj(3) = -1.5E-3; %in meters
 
 % lateral random locations
 x = round(rand([1,Np])*N/4*3-N/8*3);
 y = round(rand([1,Np])*N/4*3-N/8*3);
-
 % Put the first particle in a specific x-y-position 
-x(1)=0;
-y(1)=0;
+x(1) = 0;
+y(1) = 0;
+% x(1) = -round(N*21/128)-10;
+% y(1) = -round(N*21/128)-40;
+% x(2) = round(N*21/128)-20;
+% y(2) = round(N*21/128)+30;
+% x(3) = -round(N*21/128)-40;
+% y(3) = -40;
 
 %Esave = zeros(Hsize,Hsize,length(z));
 %% Mie solver
 % assuming x-polarized illumination
 % generating total field
-Etot = 0;
+Field = 0;
 % E = Mie_x_radiation2(n1, n2, d(1), lambda, N, dpix, z_obj(1), [ x(1), y(1)], 4);
 % Etot = Etot+E(:,:,1).*exp(-1i*k*z_obj(1));
 
+
 for p = 1:Np
     E = MieField(n1, n2, d(p), lambda, N, dpix, z_obj(p), [ x(p), y(p)], 4);
-    Ex = E(:,:,1);
+    Ex = E(:,:,1).*exp(-1i*k*z_obj(p));
+%     if maskon==true
+%         Ex2 = propagate(Ex,lambda/n1,-z_obj(p),ps/mag,'zpad',zpad,'cpu');
+%         Ex = propagate(Ex2,lambda/n1,z_obj(p),ps/mag,'zpad',zpad,'mask',maskpadded,'cpu');
+%     end
+    
+    Ex2 = propagate(Ex,lambda/n1,-abs(z_obj(p)),ps/mag,'zpad',zpad);
     if maskon==true
-        Ex2 = propagate(Ex,lambda/n1,-z_obj(p),ps/mag,'zpad',N*2,'cpu');
-        Ex = propagate(Ex2,lambda/n1,z_obj(p),ps/mag,'zpad',N*2,'mask',maskpadded,'cpu');
+        Ex = propagate(Ex2,lambda/n1,-z_obj(p),ps/mag,'zpad',zpad,'mask',maskpadded);
+    else
+        Ex = propagate(Ex2,lambda/n1,-z_obj(p),ps/mag,'zpad',zpad);
+%         mask = 1;
     end
     
 %     t = [1 0 0; 0 1 0; 1];
@@ -87,18 +111,26 @@ for p = 1:Np
 %     Etmp = imtransform(E(:,:,1), t_translate,...
 %         'XData',[1 N],'YData',[1 N]);
     
-    Etot = Etot+Ex.*exp(-1i*k*z_obj(p));
+    % record total field for comparison.
+    Field = Field+Ex;
     
 end
 
-% record total field for comparison.
-Field = Etot;
-% generating hologram
+% Generating hologram
 Holo = 1+2*real(Field)+abs(Field).^2;
+Holo_fft = fftshift(fft2(Holo));
+HoloMasked = ifft2(ifftshift(Holo_fft.*mask));
+
+theta = atan(ps*N/2/max(z_obj));
+NA = n1*sin(theta);
+lat_res = lambda/NA % lateral resolution
+DOF = lambda/NA^2 % depth of focus
 
 % set(0,'DefaultFigureWindowStyle','docked') %Dock all figures
-figure; imagesc(Holo,[0 max(Holo(:))]); axis image; colormap gray; colorbar; axis ij;
+figure; imagesc(Holo,[0 max(Holo(:))]); colormap gray; colorbar; axis image; axis ij;
+% if maskon == true
+%     figure; imagesc(mask); colormap gray; colorbar; axis image; axis ij;
+% end
 
-fn = ['Mie',num2str(N),'px_',num2str(Np),'part_',num2str(round(n1*100)),'n1_',num2str(round(n2*100)),'n2'];
-save(fn, 'Holo', 'Field', 'z_obj', 'x', 'y', 'd', 'dpix', 'lambda', 'n1', 'n2', 'n3', 'zmin', 'zmax', 'mag', 'ps');
+save(fn, 'Holo', 'Field', 'z_obj', 'x', 'y', 'd', 'dpix', 'lambda', 'n1', 'n2', 'n3', 'zmin', 'zmax', 'mag', 'ps', 'mask', 'maskpadded', 'theta', 'NA', 'DOF');
 toc
