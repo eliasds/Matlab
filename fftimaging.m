9ab68a7e9c1b24aa8cf03cfbff5b443b0f225296
eliasds <eliasds@gmail.com>
2016-02-24 11:23:11 -0800

    fftimaging.m: improved contrast and display histogram

--- fftimaging.m
+++ fftimaging.m
@@ -1,22 +1,24 @@
-filename = 'Basler_acA2040-25gm.tiff';
+filenameinit = 'Basler_.tiff';
 
-[pathstr, filename, ext] = fileparts(filename);
-filename = strrep(filename, '*', '');
-filesort = dir([filename,'*',ext]);
-numfiles = numel(filesort);
-filename = filesort(numfiles).name;
-[pathstr, filename, ext] = fileparts(filename);
+[pathstr, filenameinit, ext] = fileparts(filenameinit);
+filenameinit = strrep(filenameinit, '*', '');
+filesort = dir([filenameinit,'*',ext]);
+filename = filesort(1).name;
+[~, filename, ~] = fileparts(filename);
 filename = filename(1:end-4);
-filesort = dir([filename,'*',ext]);
-numfiles = numel(filesort);
 
-figure(123);
-for L = 1:9999
-    figure(123);
+fignum = 124;
+figure(fignum);
+while 1
     filesort = dir([filename,'*',ext]);
     numfiles = numel(filesort);
-    newfilename = [filename,num2str(numfiles-1,'%0.4u'),ext];
+    newfilename = [filename,num2str(numfiles,'%0.4u'),ext];
     Holo = imread(newfilename);
-    imagesc(log10(abs(fftshift(fft2(Holo)))),[4 8]);colormap gray;axis image; axis xy; title(newfilename);
-    drawnow
+    dynrange0 = min(Holo(:));
+    dynrange1 = max(Holo(:)); %max value just over 255 optimizes dynamic range
+    HoloFFT = log10(abs(fftshift(fft2(Holo))));
+    HoloFFT = (HoloFFT - min(HoloFFT(:)))/(max(HoloFFT(:))-min(HoloFFT(:)));
+    HoloFFT_adapthisteq = adapthisteq(HoloFFT);
+%     figure(fignum);imagesc(HoloFFT_adapthisteq);colormap gray;axis image; axis xy; title([filenameinit,num2str(numfiles,'%0.4u'),'  Dyn Range: ',num2str(dynrange0),'-',num2str(dynrange1)]);drawnow
+    figure(fignum);subplot(3,4,[1:3 5:7 9:11]);imagesc(HoloFFT_adapthisteq,[0.35 1]);colormap gray;axis image; axis xy; title([filenameinit,num2str(numfiles,'%0.4u'),'  Dyn Range: ',num2str(dynrange0),'-',num2str(dynrange1)]);subplot(3,4,4);histogram(Holo(:));axis off;title('Histogram');drawnow
 end
