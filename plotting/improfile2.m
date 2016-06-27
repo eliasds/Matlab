2e469890d2914681d73ec694ac557e4a086ae93b
eliasds <eliasds@gmail.com>
2016-06-27 09:24:43 -0700

    improfile2: optionize averaging and position

--- plotting/improfile2.m
+++ plotting/improfile2.m
@@ -1,11 +1,12 @@
-function [handlefig, handleimg, handlex, handley] = improfile2( img )
+function [handlefig, handleimg, handlex, handley, xprofile, yprofile] = improfile2( img, ncent, mcent, proflength )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
 [m,n] = size(img);
-mcent = round(size(img)/2);
-ncent = mcent(2);
-mcent = mcent(1);
+iter = 0; % 5 makes smoother graphs;
+% mcent = round(size(img)/2);
+% ncent = mcent(2); % x-position
+% mcent = mcent(1); % y-position
 
 handlefig = figure;
 handleimg = subplot(9,8,[1:7,9:15,17:23,25:31,33:39,41:47,49:55]);
@@ -17,14 +18,21 @@ hold off
 
 xprofile = improfile(img,[1 n],[mcent mcent]);
 yprofile = improfile(img,[ncent ncent],[1 m]);
-for L = 1:5
+for L = 1:iter
     xprofile = xprofile + improfile(img,[1 n],[mcent-1*L mcent-1*L]);
     xprofile = xprofile + improfile(img,[1 n],[mcent+1*L mcent+1*L]);
     yprofile = yprofile + improfile(img,[ncent-1*L ncent-1*L],[1 m]);
     yprofile = yprofile + improfile(img,[ncent+1*L ncent+1*L],[1 m]);
 end
+if isequal(L,[])
+    L = 0;
+end
 xprofile = xprofile / (2*L+1);
 yprofile = yprofile / (2*L+1);
+
+%Set Length of Profile
+xprofile = xprofile(ncent-proflength(1)/2:ncent+proflength(1)/2);
+yprofile = yprofile(mcent-proflength(2)/2:mcent+proflength(2)/2);
 
 minprofile = min(min([xprofile, yprofile]));
 maxprofile = max(max([xprofile, yprofile]));
